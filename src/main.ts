6762ddbb6a1fd401253bd99359be6553ecc6e8db
omnyx2 <omnyx2@gmail.com>
2022-06-09 20:21:18 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create(AppModule)
+
   const options = new DocumentBuilder()
     .setTitle('Posts Container')
     .setDescription('The Post API description')
     .setVersion('1.0')
     .addTag('Posts')
     .addBearerAuth()
-    .build();
+    .build()
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-  app.enableCors();
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+  app.enableCors()
 
-  
-
-  await app.listen(5000);
-
+  await app.listen(5000)
 }
-bootstrap();
+bootstrap()
